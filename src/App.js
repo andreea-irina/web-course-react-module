0cc76f188db49be84cd02e5d4b1729790d0b1e2b
Andreea Miclaus <48680797+andreea-irina@users.noreply.github.com>
2024-07-09 15:38:27 +0300

    added examples for the session

--- src/App.js
+++ src/App.js
@@ -2,35 +2,31 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import UserList from "./session34/examples/UserList";
 import UserAddForm from "./session34/examples/UserAddForm";
-import { Button } from "react-bootstrap";
-import LiveCodeApp from "./session34/liveCode/App";
 
 function App() {
-  // const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("https://jsonplaceholder.typicode.com/users")
-  //     .then((response) => response.json())
-  //     .then((data) => setUsers(data));
-  // }, []);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
 
-  // const addUser = (user) => {
-  //   setUsers([...users, user]);
-  // };
+  const addUser = (user) => {
+    setUsers([...users, user]);
+  };
 
-  // const deleteUser = (id) => {
-  //   setUsers(users.filter((user) => user.id !== id));
-  // };
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
 
   return (
     <div className="app-content">
-      {/* <div style={{ display: "flex", gap: "30px" }}>
+      <div style={{ display: "flex", gap: "30px" }}>
         <UserList users={users} onDeleteUser={deleteUser} />
 
         <UserAddForm onAddUser={addUser} />
-      </div> */}
-
-      <LiveCodeApp />
+      </div>
     </div>
   );
 }
